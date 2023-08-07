3c44be4b271e832b05d75115bb11df56afee85e6
babak zarrinbal <babak.zarrinbal@gmail.com>
2023-08-07 15:19:49 +0200

    init

---
+++ src/App2.vue
@@ -0,0 +1,21 @@
+<template>
+  <p>babak test is here </p>
+</template>
+
+<script>
+
+export default {
+  name: 'AppTwo',
+}
+</script>
+
+<style>
+#app2 {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+// custom styles
+import "./styles/global-style.scss";
+import "./styles/vue-transitions.scss";
+
+import { createApp } from "vue";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
+import "bootstrap/dist/css/bootstrap-reboot.min.css";
+import App from "./App.vue";
+
+createApp(App).mount("#app");
